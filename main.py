5c444eadeaa9e8f93d20695bdb1b80e2106c062b
Mark Ivanov <markmipt@gmail.com>
2018-02-06 17:27:00 +0300

    Fix lightgbm params

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def process_file(args):
     df1 = prepare_dataframe_xtandem(fname, decoy_prefix='DOUBLE_')
     df1 = calc_PEP(df1)
     # df1_f2 = aux.filter(df1, fdr=0.01, key='expect', is_decoy='decoy', reverse=False, remove_decoy=True, ratio=1.0)
-    df1_f2 = aux.filter(df1, fdr=0.01, key='PEP', is_decoy='decoy2', reverse=False, remove_decoy=True, ratio=0.333)
+    df1_f2 = aux.filter(df1, fdr=0.01, key='PEP', is_decoy='decoy2', reverse=False, remove_decoy=True, ratio=1.0)
     real_false = df1_f2[df1_f2['protein'].apply(lambda x: all(z.startswith('DECOY_') for z in x))].shape[0]
     real_target = df1_f2.shape[0]
     print(real_false * 100 / real_target, real_target)
