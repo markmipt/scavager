0811f73cc0fd5a0f940ac2bbd64b44add5f1e0d3
Mark Ivanov <markmipt@gmail.com>
2018-02-08 18:06:16 +0300

    code cleaning

--- utils.py
+++ utils.py
@@ -138,7 +138,6 @@ def split_decoys(df, decoy_prefix):
     random.seed(SEED)
     all_decoys_2 = set(random.sample(all_decoys, int(len(all_decoys)/2)))
     df['decoy2'] = df['protein'].apply(is_decoy_2, decoy_set=all_decoys_2)
-    # df['decoy1'] = df['decoy'].apply(lambda x: True if x and random.random() >= 0.5 else False)
     df['decoy1'] = df.apply(lambda x: x['decoy'] and not x['decoy2'], axis=1)
     return df, all_decoys_2
 
@@ -234,28 +233,16 @@ def get_Y_array(df):
     return df.loc[:, 'decoy1'].values
 
 def get_gbm_model(df):
-    # train, test = train_test_split(df, test_size = 0.5)
-#     print(type(feature_columns))
-    # if type(feature_columns)==bool:
     feature_columns = get_features(df)
-    # x_train = get_X_array(train, feature_columns)#train.iloc[:, 0:-1].values
-    # y_train = get_Y_array(train)#train['target'].values
-    x_train = get_X_array(df, feature_columns)#train.iloc[:, 0:-1].values
-    y_train = get_Y_array(df)#train['target'].values
-    # x_test = get_X_array(test, feature_columns)#test.iloc[:, 0:-1].values
-    # y_test = get_Y_array(test)#test['target'].values
-    # print(np.sum(y_train), len(y_train))
-    # create dataset for lightgbm
+    x_train = get_X_array(df, feature_columns)
+    y_train = get_Y_array(df)
     lgb_train = lgb.Dataset(x_train, y_train)
-    # lgb_eval = lgb.Dataset(x_test, y_test, reference=lgb_train)
 
-    # specify your configurations as a dict
     params = {
         'task': 'train',
         'boosting_type': 'gbdt',
         'objective': 'regression',
         'metric': {'l2', 'auc'},
-#         'metric': 'multi_logloss',
         'num_leaves': 10,
         'learning_rate': 0.1,
         'feature_fraction': 0.95,
@@ -264,19 +251,9 @@ def get_gbm_model(df):
         'bagging_seed': SEED,
         'bagging_freq': 5,
         'verbose': 0,
-       'min_data_in_bin': 1,
-       'min_data': 1,
-#        'min_sum_hessian_in_leaf': 0.001,
+        'min_data_in_bin': 1,
+        'min_data': 1
     }
-
-#     print('Start training...')
-    # train
-    # gbm = lgb.train(params,
-    #                 lgb_train,
-    #                 num_boost_round=1000,
-    #                 valid_sets=lgb_eval,
-    #                 early_stopping_rounds=10, verbose_eval=1)
-    
     
     cv_result_lgb = lgb.cv(params, 
                         lgb_train, 
@@ -288,20 +265,9 @@ def get_gbm_model(df):
                         show_stdv=False, seed=SEED)
     
     num_boost_rounds_lgb = len(cv_result_lgb['auc-mean'])
-    # print(cv_result_lgb['auc-mean'])
     print('num_boost_rounds_lgb=' + str(num_boost_rounds_lgb))
     # train model
     gbm = lgb.train(params, lgb_train, num_boost_round=num_boost_rounds_lgb)
-    
-#     print('Save model...')
-#     #save model to file
-#     gbm.save_model('model.txt')
-
-#     print('Start predicting...')
-    # predict
-    # y_pred = gbm.predict(X_test)
-    # eval
-#     print('The rmse of prediction is:', mean_squared_error(y_test, y_pred) ** 0.5)
     return gbm
 
 def calc_PEP(df):
