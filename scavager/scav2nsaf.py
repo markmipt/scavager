e241da121cb188fd622ab7505988cb6489c884d9
Lev Levitsky <lev.levitsky@phystech.edu>
2019-04-16 02:11:47 +0300

    Add union option in scav2nsaf

--- scavager/scav2nsaf.py
+++ scavager/scav2nsaf.py
@@ -4,14 +4,31 @@ import argparse
 import pkg_resources
 import pandas as pd
 import ast
-import subprocess
 import numpy as np
 from itertools import chain
 from pyteomics import fasta
+import os
+
+def read_table(z, allowed_peptides, allowed_prots, label=None):
+    if label is None:
+        label = z.replace('_proteins.tsv', '')
+    if isinstance(z, str):
+        df = pd.read_csv(z.replace('_proteins.tsv', '_PSMs_full.tsv'), sep='\t')
+    else:
+        df = pd.concat([pd.read_csv(f.replace('_proteins.tsv', '_PSMs_full.tsv'), sep='\t') for f in z])
+
+    df = df[df['peptide'].apply(lambda z: z in allowed_peptides)]
+    df['count'] = df.groupby('peptide')['peptide'].transform('count')
+    df = df.sort_values('q', ascending=True).drop_duplicates(['peptide'])
+    df[label] = df['count']
+    df['protein'] = df['protein'].apply(lambda z: [u for u in ast.literal_eval(z) if u in allowed_prots])
+    df = df[df['protein'].apply(lambda z: len(z)>0)]
+    df = df[['peptide', 'protein', label]]
+    return df
 
 def run():
     parser = argparse.ArgumentParser(
-        description='calc NSAF for scavager results',
+        description='calculate NSAF for scavager results',
         epilog='''
 
     Example usage
@@ -25,6 +42,9 @@ def run():
     parser.add_argument('-S2', nargs='+', help='input files for S2 sample')
     parser.add_argument('-S3', nargs='+', help='input files for S3 sample')
     parser.add_argument('-S4', nargs='+', help='input files for S4 sample')
+    parser.add_argument('-u', '--union',  help='pool the files together for the samples', action='store_true')
+    parser.add_argument('-a', '--autolabel', help='in union mode, derive sample labels from common name prefixes',
+        action='store_true')
     parser.add_argument('-db', help='path to fasta file', required=True)
     parser.add_argument('-out', help='name of nsaf output file', default='nsaf_out.txt')
     parser.add_argument('-version', action='version', version='%s' % (pkg_resources.require("scavager")[0], ))
@@ -39,43 +59,39 @@ def run():
     for sample_num in ['S1', 'S2', 'S3', 'S4']:
         if args[sample_num]:
             for z in args[sample_num]:
-                df0 = pd.read_table(z)
+                df0 = pd.read_csv(z, sep='\t')
                 allowed_prots.update(df0['dbname'])
 
 
     for sample_num in ['S1', 'S2', 'S3', 'S4']:
         if args[sample_num]:
             for z in args[sample_num]:
-                df0 = pd.read_table(z.replace('_proteins.tsv', '_peptides.tsv'))
+                df0 = pd.read_csv(z.replace('_proteins.tsv', '_peptides.tsv'), sep='\t')
                 allowed_peptides.update(df0['peptide'])
 
 
     for sample_num in ['S1', 'S2', 'S3', 'S4']:
         if args[sample_num]:
-            for z in args[sample_num]:
-                label = z.replace('_proteins.tsv', '')
-                df1 = pd.read_table(z.replace('_proteins.tsv', '_PSMs_full.tsv'))
-                # df1 = pd.read_table(z.replace('_proteins.tsv', '_PSMs.tsv'))
-                df1 = df1[df1['peptide'].apply(lambda z: z in allowed_peptides)]
-                # print(df1.shape)
-                # print(z.replace('_proteins.tsv', '_PSMs_full.tsv'))
-                # print(df1.columns)
-                # df1['peptide'] = df1.apply(lambda z: z['peptide'] + str(z['assumed_charge']), axis=1)
-                # df1 = df1.sort_values('MS1Intensity', ascending=False).drop_duplicates(['peptide'])
-                # df1['peptide'] = df1['peptide'].apply(lambda z: z[:-1])
-                df1['count'] = df1.groupby('peptide')['peptide'].transform('count')
-                # print(df1['count'])
-                df1 = df1.sort_values('q', ascending=True).drop_duplicates(['peptide'])
-                df1[label] = df1['count']
-                # df1[label] = df1[label].replace([0, 0.0], np.nan)
-                df1['protein'] = df1['protein'].apply(lambda z: [u for u in ast.literal_eval(z) if u in allowed_prots])
-                df1 = df1[df1['protein'].apply(lambda z: len(z)>0)]
-                df1 = df1[['peptide', 'protein', label]]
+            if not args['union']:
+                for z in args[sample_num]:
+                    df1 = read_table(z, allowed_peptides, allowed_prots)
+                    if df_final is False:
+                        df_final = df1
+                    else:
+                        df_final = df_final.reset_index().merge(df1.reset_index(), on='peptide', how='outer')#.set_index('peptide')
+                        df_final.protein_x.fillna(value=df_final.protein_y, inplace=True)
+                        df_final['protein'] = df_final['protein_x']
+                        df_final = df_final.drop(columns=['protein_x', 'protein_y', 'index_x', 'index_y'])
+            else:
+                if args['autolabel']:
+                    label = os.path.commonprefix(args[sample_num]).rstrip('_')
+                else:
+                    label = sample_num
+                df1 = read_table(args[sample_num], allowed_peptides, allowed_prots, label=label)
                 if df_final is False:
                     df_final = df1
                 else:
                     df_final = df_final.reset_index().merge(df1.reset_index(), on='peptide', how='outer')#.set_index('peptide')
-                    # df_final = df_final.merge(df1, on='peptide', how='outer')
                     df_final.protein_x.fillna(value=df_final.protein_y, inplace=True)
                     df_final['protein'] = df_final['protein_x']
                     df_final = df_final.drop(columns=['protein_x', 'protein_y', 'index_x', 'index_y'])
