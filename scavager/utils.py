a5b54ffecb92127fb8d740885c292ee71ab909b1
markmipt <markmipt@gmail.com>
2025-08-07 19:47:59 +0300

    Fix for expect value binsize

--- scavager/utils.py
+++ scavager/utils.py
@@ -686,7 +686,14 @@ def calc_PEP(df, pep_ratio=1.0, reduced=False):
     if rbin_s and abs((rbin - rbin_s) / rbin_s) > 1.0:
         rbin = rbin_s * 1.05
     rbin += 1.5 * binsize
+
+
     logger.debug('cbins: lbin = %s, rbin = %s, binsize = %s', lbin, rbin, binsize)
+    num_bins = ((rbin + 2 * binsize) - lbin) / binsize
+    if num_bins >= 100:
+        binsize = ((rbin + 2 * binsize) - lbin) / 100
+        logger.debug('cbins: lbin = %s, rbin = %s, binsize = %s', lbin, rbin, binsize)
+
     cbins = np.arange(lbin, rbin + 2 * binsize, binsize)
 
     H1, b1 = np.histogram(df0_d['ML score'].values, bins=cbins)
